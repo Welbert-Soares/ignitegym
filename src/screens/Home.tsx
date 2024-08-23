ed16041a9b0ceb68df8120dad78ff6a1ad4d390b
Welbert-Soares <welbertsoares6@gmail.com>
2024-08-23 17:15:05 -0300

    utilizando o bottom tab

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,8 +2,8 @@ import { Center, Text } from "@gluestack-ui/themed"
 
 export function Home() {
   return (
-    <Center>
-      <Text></Text>
+    <Center flex={1}>
+      <Text color="$white">Home</Text>
     </Center>
   )
 }
