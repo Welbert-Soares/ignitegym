20bef0cef691baa2dc804b81474bc3fc98804820
Welbert-Soares <welbertsoares6@gmail.com>
2024-08-26 17:20:39 -0300

    estiizando group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,27 @@
-import { VStack } from "@gluestack-ui/themed"
+import { HStack, VStack } from "@gluestack-ui/themed"
 
 import { HomeHeader } from "@components/HomeHeader"
 import { Group } from "@components/Group"
+import { useState } from "react"
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState("costas")
+
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="Costas" isActive={false} />
+      <HStack>
+        <Group
+          name="Costas"
+          isActive={groupSelected === "costa"}
+          onPress={() => setGroupSelected("costa")}
+        />
+        <Group
+          name="Ombro"
+          isActive={groupSelected === "ombro"}
+          onPress={() => setGroupSelected("ombro")}
+        />
+      </HStack>
     </VStack>
   )
 }
