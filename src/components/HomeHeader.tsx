7b8f6c6125bb9e8b14795bc51a67cffbc5f340fd
Welbert-Soares <welbertsoares6@gmail.com>
2024-08-26 16:54:58 -0300

    componente userPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,15 @@
 import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed"
+import { UserPhoto } from "./UserPhoto"
 
 export function HomeHeader() {
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://github.com/Welbert-Soares.png" }}
+        alt="Imagem do usuário"
+        w="$16"
+        h="$16"
+      />
       <VStack>
         <Text color="$gray100" fontSize="$sm">
           Olá,
---
+++ src/components/UserPhoto.tsx
@@ -0,0 +1,17 @@
+import { Image } from "@gluestack-ui/themed"
+import { ComponentProps } from "react"
+
+type Props = ComponentProps<typeof Image>
+
+export function UserPhoto({ ...rest }: Props) {
+  return (
+    <Image
+      rounded="$full"
+      borderWidth="$2"
+      borderColor="$gray400"
+      backgroundColor="$gray500"
+      gap="$4"
+      {...rest}
+    />
+  )
+}
