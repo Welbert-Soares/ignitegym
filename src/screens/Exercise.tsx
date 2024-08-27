978a9bde7cd48f36f56def2570e6af866ab63725
Welbert-Soares <welbertsoares6@gmail.com>
2024-08-26 22:23:13 -0300

    criando botao voltar

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,10 +1,24 @@
-import { Center, Text } from "@gluestack-ui/themed"
-import React from "react"
+import { TouchableOpacity } from "react-native"
+import { ArrowLeft } from "lucide-react-native"
+import { VStack, Icon } from "@gluestack-ui/themed"
+import { useNavigation } from "@react-navigation/native"
+
+import { AppNavigatorRoutesProps } from "../../routes/app.routes"
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  const handleGoBack = () => {
+    navigation.goBack()
+  }
+
   return (
-    <Center flex={1}>
-      <Text color="$white">Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px="$8" bg="$gray600" pt="$12">
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={ArrowLeft} color="$green500" size="xl" />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   )
 }
