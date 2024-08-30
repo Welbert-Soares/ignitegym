bce4e1e589ee91ea79abd99758d317305345a08d
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-08-30 18:13:35 -0300

    tratando erros da API

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,3 @@
-import { Alert } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import {
   Center,
@@ -7,19 +6,22 @@ import {
   Text,
   VStack,
   ScrollView,
+  useToast,
 } from "@gluestack-ui/themed"
 import { useForm, Controller } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 
-import axios from "axios"
 import { api } from "@services/api"
 
 import BackgroundImg from "@assets/background.png"
 import Logo from "@assets/logo.svg"
 
+import { AppError } from "@utils/AppError"
+
 import { Input } from "@components/Input"
 import { Button } from "@components/Button"
+import { ToastMessage } from "@components/ToastMessage"
 
 type FormDataProps = {
   name: string
@@ -42,6 +44,8 @@ const signUpSchema = yup.object({
 })
 
 export const SignUp = () => {
+  const toast = useToast()
+
   const {
     control,
     handleSubmit,
@@ -60,9 +64,22 @@ export const SignUp = () => {
       const response = await api.post("/users", { name, email, password })
       console.log(response.data)
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        Alert.alert(error.response?.data.message)
-      }
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta."
+
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            action="error"
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      })
     }
   }
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
 
+import { AppError } from "@utils/AppError";
+
 const api = axios.create({
   baseURL: 'http://192.168.2.67:3333'
 })
 
-api.interceptors.request.use((config) => {
-  console.log('interceptor', config)
-  return config
-
-}, (error) => {
-  return Promise.reject(error)
+api.interceptors.response.use(response => response, error => {
+  if (error.response && error.response.data) {
+    return Promise.reject(new AppError(error.response.data.message))
+  } else {
+    return Promise.reject(error)
+  }
 })
 
 export { api }
