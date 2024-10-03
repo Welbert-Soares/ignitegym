91a4721951f70db5f0c867cb7f9594126de449b6
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-10-03 19:16:16 -0300

    Refactor AuthContext to load user data from AsyncStorage on component mount

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,12 +7,15 @@ import { AppRoutes } from "./app.routes"
 import { useAuth } from "@hooks/useAuth"
 
 import { gluestackUIConfig } from "../../config/gluestack-ui.config"
+import { Loading } from "@components/Loading"
 
 export function Routes() {
-  const { user } = useAuth()
+  const { user, isLoadingUserStorageData } = useAuth()
 
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
+
+  if (isLoadingUserStorageData) return <Loading />
 
   return (
     <Box flex={1} bg="$gray700">
