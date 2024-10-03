50c7e1393ab7df2103ca4a2331c9b3f8b19887fa
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-10-03 20:10:02 -0300

    Refactor AuthContext to save user data and token to state and storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react"
 
+import { storageAuthTokenSave } from "@storage/storageAuthToken"
 import {
   storageUserGet,
   storageUserRemove,
@@ -28,6 +29,23 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true)
+
+      // anexa o token de autenticação nas requisições
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+
+      await storageUserSave(userData)
+      await storageAuthTokenSave(token)
+      setUser(userData)
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post("/sessions", {
@@ -36,8 +54,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       })
 
       if (data.user && data.token) {
-        setUser(data.user)
-        storageUserSave(data.user)
+        storageUserAndToken(data.user, data.token)
       }
     } catch (error) {
       throw error
